b3b37ddf0c5a0b7737288a30c6ba828903bb146d
Ailson Roberto <robertofeitosa@gmail.com>
2020-11-17 00:26:08 -0300

    first commit

---
+++ src/components/Shop-Item.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="Item">
+
+        <img :src="item.imageSrc" :alt="item.name" class="ItemImage">
+        <div class="ItemDetails">
+            <p><strong>{{item.name}}</strong></p>
+            <p>Preço: <strong>R$ {{item.price}}</strong></p>
+        </div>
+
+        <!-- Button component -->
+        <Shop-Button-Add
+                @button-clicked="addToCart(item)"
+                :item="item">
+            <p>Adicionar ao carrinho</p>
+        </Shop-Button-Add>
+
+    </div>
+</template>
+
+<script>
+import ShopButtonAdd from './Shop-Button-Add.vue'
+
+    export default {
+        name: 'Shop-Item',
+        components: { ShopButtonAdd },
+        props: ['item'],
+        data() {
+            return {}
+        },
+
+        methods: {
+            addToCart(item) {
+                this.$emit('update-cart', item)
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
